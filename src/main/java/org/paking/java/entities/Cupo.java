5c489be8f5a80b620ccaef16b5eb75b298a5b863
duvan <duvan060605@gmail.com>
2024-02-26 14:20:01 -0500

    Cambio 26feb

--- src/main/java/org/paking/java/entities/Cupo.java
+++ src/main/java/org/paking/java/entities/Cupo.java
@@ -2,4 +2,16 @@ package org.paking.java.entities;
 
 public class Cupo {
 
+    public char nombre;
+
+    //Constructor por defecto 
+    public Cupo() {
+    }
+
+    //Constructor parametrizado
+    public Cupo(char nombre) {
+        this.nombre = nombre;
+    }
+
+    
 }
--- src/main/java/org/paking/java/entities/Pago.java
+++ src/main/java/org/paking/java/entities/Pago.java
@@ -1,5 +1,28 @@
 package org.paking.java.entities;
+import java.time.LocalDateTime;
 
 public class Pago {
 
+    //Atributos intrinsecos
+    public LocalDateTime fechaInicio;
+    public LocalDateTime fechaHoraFin;
+    public Double valor;
+
+    //Atributos de relacion
+    public Vehiculo vehiculo;
+    public Cupo cupo;
+
+    //Constructor por defecto
+    public Pago() {
+    }
+    
+    //Constructor parametrizado
+    public Pago(LocalDateTime fechaInicio, LocalDateTime fechaHoraFin, Double valor, Vehiculo vehiculo, Cupo cupo) {
+        this.fechaInicio = fechaInicio;
+        this.fechaHoraFin = fechaHoraFin;
+        this.valor = valor;
+        this.vehiculo = vehiculo;
+        this.cupo = cupo;
+    }
+    
 }
